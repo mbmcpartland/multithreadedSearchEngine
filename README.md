229eaeaa8eff2f7b3f44bedea59addb697a35c9b
sjengle <sjengle@10.10.10.194>
2017-01-30 13:37:32 -0800

    Initializing project template.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# template-project
+See the [Projects](https://github.com/usf-cs212-2017/lectures/wiki/Projects) page for details.
---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		// TODO
+		System.out.println(Arrays.toString(args));
+
+	}
+
+}
